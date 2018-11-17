c829479809e48fa1d2925f4dc2d2ef6229652788
iTitus <iTitus@users.noreply.github.com>
2018-11-17 23:29:05 +0100

    Add portrait generator

--- ow-portrait-viewer/viewer.js
+++ ow-portrait-viewer/viewer.js
@@ -4,7 +4,7 @@ const SVG = "http://www.w3.org/2000/svg";
 const CIRCUMFERENCE = 100;
 
 $(function () {
-    const parent = $("#portraits");
+    const parent = $("#portrait-viewer");
     const start_string = "0x0250000000000";
     const start = 0x918; //0x922;
     const end = 0x9d0; //0x9cd
@@ -13,7 +13,7 @@ $(function () {
 
         const div_element = $("<div>", {
             "id": hex_string,
-            "class": "portrait"
+            "class": "portrait-box"
         });
         {
             const div_img_container = $("<div>", {
@@ -67,7 +67,7 @@ $(function () {
                     {
                         const endorsementIconDiv = $("<div>", {
                             class: "endorsement-icon",
-                            style: "background-image:url(https://d3hmvhl7ru3t12.cloudfront.net/svg/icons/endorsement-frames-be58f32488898c106f2ad8b1c3012dbce42d19b758c4b18bf2afbe91fc8f1d2cc83a0de433321792ff0fb3a1f66d37ded31598bd538838b30fa4537db617926e.svg#_" + level + ")"
+                            style: "background-image: url(https://d3hmvhl7ru3t12.cloudfront.net/svg/icons/endorsement-frames-be58f32488898c106f2ad8b1c3012dbce42d19b758c4b18bf2afbe91fc8f1d2cc83a0de433321792ff0fb3a1f66d37ded31598bd538838b30fa4537db617926e.svg#_" + level + ")"
                         });
                         {
                             const endorsementIconInnerDiv = $("<div>", {
@@ -126,7 +126,78 @@ $(function () {
             }
         }
     });
+
+    const portraitForm = $("#portrait-form");
+    portraitForm.submit(function (e) {
+        const level = parseInt($("#portrait-form-level").val());
+        if (isNaN(level)) {
+            return false;
+        }
+
+        e.preventDefault();
+        const border_type = getBorderType(level);
+        const border_index = getBorderIndex(level);
+        const stars_index = getStarsIndex(level);
+        const level_text = getLevelText(level);
+
+        const portraitDiv = $("#portrait");
+        portraitDiv.empty();
+
+        const div_parent = $("<div>", {
+            class: "portrait-parent"
+        });
+        {
+            const div_border = $("<div>", {
+                class: "portrait-border",
+                style: "background-image: url(img/portraits/" + border_type + "/border_" + border_index + ".png)"
+            });
+            {
+                $("<div>", {
+                    class: "portrait-level-text"
+                }).text(level_text).appendTo(div_border);
+
+                if (stars_index > 0) {
+                    $("<div>", {
+                        class: "portrait-stars",
+                        style: "background-image: url(img/portraits/" + border_type + "/stars_" + stars_index + ".png)"
+                    }).appendTo(div_border);
+                }
+            }
+            div_parent.append(div_border);
+        }
+        portraitDiv.append(div_parent);
+    });
 });
+
+function getBorderType(level) {
+    const border_type = (level - 1) / 600;
+    if (border_type < 1) {
+        return "bronze";
+    } else if (border_type < 2) {
+        return "silver";
+    } else if (border_type < 3) {
+        return "gold";
+    } else if (border_type < 4) {
+        return "platinum";
+    }
+    return "diamond";
+}
+
+function getBorderIndex(level) {
+    return Math.floor(((level - 1) % 100) / 10);
+}
+
+function getStarsIndex(level) {
+    if (level > 5900) {
+        return 5;
+    }
+    return Math.floor(((level - 1) % 600) / 100);
+}
+
+function getLevelText(level) {
+    return ((level - 1) % 100) + 1;
+}
+
 /*
 For async loading
 var image = document.images[0];
